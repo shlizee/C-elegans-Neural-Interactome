9fcc3047bb110ed08e92ad3a65c357964633801a
jimin9292 <jk55@u.washington.edu>
2015-09-16 22:48:15 -0700

    Adding the pre-existing scripts
    Adding the pre-existing scripts

---
+++ CElegansFull.py
@@ -0,0 +1,330 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+import time
+
+from scipy import integrate, signal
+
+WrittenBy = 'Jimin Kim'
+Email = 'jk55@u.washington.edu'
+Version = '0.3'
+
+# In[2]:
+
+# Number of Neurons 
+N = 279
+# ----------------------------------------------------------------------------------------------------------------------
+# Cell membrane conductance (pS)
+Gc = 0.1
+#------------------------------------------------------------------------------------------------------------------------
+# Cell Membrane Capacitance
+C = 0.01
+# -----------------------------------------------------------------------------------------------------------------------
+# Gap Junctions (Electrical, 279*279)
+ggap = 1.0
+G = sio.loadmat('Gg.mat')
+Gg = G['Gg']
+# -----------------------------------------------------------------------------------------------------------------------
+# Synaptic connections (Chemical, 279*279)
+gsyn = 1.0
+S = sio.loadmat('Gs.mat')
+Gs = S['Gs']
+# ----------------------------------------------------------------------------------------------------------------------
+# Leakage potential (mV)
+Ec = -35.0 
+# ----------------------------------------------------------------------------------------------------------------------
+# Directionality (279*1)
+E = sio.loadmat('Emask.mat')
+E = -45.0*E['E']
+EMat = np.tile(np.reshape(E, N), (N, 1))
+# ----------------------------------------------------------------------------------------------------------------------
+# Synaptic Activity Parameters 
+ar = 1.0 # Synaptic activity's rise time
+ad = 5.0 # Synaptic activity's decay time
+B = 0.125 # Width of the sigmoid (mv^-1)
+
+
+# In[3]:
+
+def VthFinder(Iext, InMask):
+
+    Gcmat = np.zeros((N, N))
+    np.fill_diagonal(Gcmat, Gc)
+
+    Ecvec = np.zeros((N,1))
+    Ecvec.fill(Ec)
+
+    M1 = -Gcmat
+    b1 = Gc * Ecvec
+
+    Ggij = np.multiply(ggap, Gg)
+    gdiag = np.zeros((N, N))
+    np.fill_diagonal(gdiag, np.diagonal(Ggij))
+    GgapSubDiag = np.subtract(Ggij, gdiag)
+    GgapSum = GgapSubDiag.sum(axis = 1)
+    GgapSumMat = np.zeros((N, N))
+    np.fill_diagonal(GgapSumMat, GgapSum)
+
+    M2 = -(np.subtract(GgapSumMat, GgapSubDiag))
+
+    Gsij = np.multiply(gsyn, Gs)
+    Seq = round((ar/(ar + 2 * ad)), 4)
+    S_eq = np.zeros((N, 1))
+    S_eq.fill(Seq)
+    Sjmat = np.zeros((N, N))
+    Sjmat.fill(Seq)
+    GSyn = np.multiply(Sjmat, Gsij)
+    sdiag = np.zeros((N, N))
+    np.fill_diagonal(sdiag, np.diagonal(GSyn))
+    GSynSubDiag = np.subtract(GSyn, sdiag)
+    GSynSum = GSynSubDiag.sum(axis = 1)
+    GSynSumMat = np.zeros((N, N))
+    np.fill_diagonal(GSynSumMat, GSynSum)
+
+    M3 = -GSynSumMat
+    b2 = np.dot(Gsij, np.multiply(S_eq, E))
+
+    InputMask = Iext * InMask
+
+    M = M1 + M2 + M3
+    b = -(b1 + b2 + InputMask)
+
+    Vth = np.linalg.solve(M, b)
+    
+    return Vth
+
+
+# In[1]:
+
+def Neuron(t, y):
+    
+    # Split the incoming values
+    
+    Vvec, SVec = np.split(y, 2)
+    
+    # Gc(Vi - Ec)
+    
+    VsubEc = np.multiply(Gc, (Vvec - Ec))
+    
+    # Gg(Vi - Vj) Computation
+    
+    Vrep = np.tile(Vvec, (N, 1))
+    GapCon = np.multiply(Gg, np.subtract(np.transpose(Vrep), Vrep)).sum(axis = 1)
+    
+    # Gs*S*(Vi - Ej) Computation
+    VsubEj = np.subtract(np.transpose(Vrep), EMat)
+    SynapCon = np.multiply(np.multiply(Gs, np.tile(SVec, (N, 1))), VsubEj).sum(axis = 1)
+    
+    # ar*(1-Si)*Sigmoid Computation
+      
+    #SynRise = np.multiply(np.multiply(ar, (1.0 - SVec)), 
+                          #np.reciprocal(1.0 + np.exp(-B*(np.subtract(Vvec, VthMat[t*1e3,:])))))
+    SynRise = np.multiply(np.multiply(ar, (np.subtract(1.0, SVec))), 
+                          np.reciprocal(1.0 + np.exp(-B*(np.subtract(Vvec, Vth)))))
+    
+    SynDrop = np.multiply(ad, SVec)    
+    
+    # Input Mask
+    
+    Input = np.multiply(Iext, InMask_row)
+    
+    # dV and dS and merge them back to dydt
+    
+    dV = (-(VsubEc + GapCon + SynapCon) + Input)/C
+    dS = np.subtract(SynRise, SynDrop)
+    
+    return np.concatenate((dV, dS))
+
+
+# In[5]:
+
+def run_Network(rhs, t_Start, t_Final, t_Delta, input_Mask, input_Magnitude, atol):
+    
+    # Time Range of Simulation
+    t0 = t_Start
+    tf = t_Final #2.0 #4.0 #8.0 #16 #32 #64
+    dt = t_Delta
+
+    #tVec = np.arange(t0, 2*tf, dt/10)
+    global nsteps
+    nsteps = np.floor((tf - t0)/dt) + 1
+
+    # Configuring the input mask
+    if input_Mask == 'PLM':
+
+        InMask = np.zeros((N, 1))
+        InMask[276] = 1
+        InMask[278] = 1
+    
+    elif input_Mask == 'ALM':
+        
+        InMask = sio.loadmat('ExtMask3.mat')
+        InMask = InMask['Ext3']
+        
+    elif input_Mask == 'RMD':
+        
+        InMask = sio.loadmat('ExtMask2.mat')
+        InMask = InMask['Ext2']
+        
+    else:
+        
+        InMask = input_Mask
+    
+    # Input signal magnitude
+    global Iext 
+    Iext = input_Magnitude
+    #Iext = 1000*signal.square(np.pi*tVec)+1000
+    
+    #Calculate V_threshold
+    #VthMat = VthFinder(Iext).transpose()
+    global Vth 
+    Vth = np.reshape(VthFinder(Iext, InMask), N)
+    global InMask_row 
+    InMask_row = np.reshape(InMask, N)
+    
+    InitCond = 10**(-4)*np.random.normal(0, 0.94, 2*N)   
+         
+    # Configuring the ODE Solver
+    r = integrate.ode(rhs).set_integrator('vode', atol = atol, min_step = dt*1e-6, method = 'bdf', with_jacobian = True)
+    r.set_initial_value(InitCond, t0)
+    
+    # Additional Python step to store the trajectories
+    t = np.zeros((nsteps, 1))
+    Traj = np.zeros((nsteps, N))
+    
+    t[0] = t0
+    Traj[0, :] = InitCond[:N]
+    
+    # Integrate the ODE(s) across each delta_t timestep
+    k = 1
+    
+    start_time = time.time()
+    
+    while r.successful() and k < nsteps:
+                 
+        r.integrate(r.t + dt)
+                 
+        t[k] = r.t
+        Traj[k, :] = r.y[:N]
+        k += 1
+    
+    end_time = time.time()
+    
+    t_Comp = end_time - start_time
+    
+    return (t, Traj, t_Comp)
+
+
+# In[6]:
+
+def plot_Colormap(Traj):
+
+    Traj = Traj.transpose()
+    #VthMatH = VthMat[:8001]
+
+    VsubVth = np.subtract(Traj, np.tile(Vth, (nsteps, 1)).transpose())
+    #VsubVth = np.subtract(Traj, VthMatH[::10,:].transpose())
+
+    # Motor Neurons 
+    Nplot = np.array([165, 153, 173, 189, 204, 219, 236, 164, 182, 196, 215, 
+                     232, 246, 151, 139, 171, 180, 187, 194, 203, 213, 218,
+                     230, 235, 167, 169, 175, 184, 191, 201, 206, 216, 222,
+                     233, 239, 242, 251])
+
+    # Subtract 1 since python matrices have 0 index
+    Nplot = np.subtract(Nplot, 1)
+
+    Motor_Vth = VsubVth[Nplot, 100:]
+    #Motor_VthT = Motor_Vth.transpose()
+    #Motor_Pure = Traj[Nplot, :]
+
+    # Perform SVD 
+    #U, s, Z = np.linalg.svd(Motor_VthT, full_matrices=True)
+
+    #MM = (np.mat(U[:,0][:,np.newaxis]) * np.mat(Z[:,0]) * s[0]) + (np.mat(U[:,1][:,np.newaxis]) * np.mat(Z[:,1]) * s[1])
+
+    #ZPLM1 = sio.loadmat('ZPLM1.mat')
+    #ZPLM1 = ZPLM1['ZPLM1']
+
+    #ZPLM2 = sio.loadmat('ZPLM2.mat')
+    #ZPLM2 = ZPLM2['ZPLM2']
+
+    # First two dominant modes of motor neurons
+    #U1 = np.matrix(Motor_VthT) * np.matrix(ZPLM1)
+    #U2 = np.matrix(Motor_VthT) * np.matrix(ZPLM2)
+    
+    fig = plt.figure(figsize=(15,10))
+    plt.pcolor(VsubVth[:, 100:], cmap='RdBu')
+    plt.colorbar()
+    plt.xlim(0, nsteps - 100)
+    plt.ylim(0,N)
+    plt.xlabel('Time (10 ms)', fontsize = 12.5)
+    plt.ylabel('Neuron Index Number', fontsize = 12.5)
+    plt.title('C.Elegans Neurons Voltage Dynamics', fontsize = 15)
+    plt.savefig('CElegansWhole')
+
+    fig = plt.figure(figsize=(15,10))
+    plt.pcolor(Motor_Vth, cmap='RdBu')
+    plt.colorbar()
+    plt.xlim(0, nsteps - 100)
+    plt.ylim(0,len(Nplot))
+    plt.xlabel('Time (10 ms)', fontsize = 12.5)
+    plt.ylabel('Motor Neuron Index Number', fontsize = 12.5)
+    plt.title('C.Elegans Motor Neurons Voltage Dynamics', fontsize = 15)
+    plt.savefig('CElegansMotor')
+
+
+# In[7]:
+
+def plot_DominantModes(Traj, t):
+
+    Traj = Traj.transpose()
+    #VthMatH = VthMat[:8001]
+
+    VsubVth = np.subtract(Traj, np.tile(Vth, (nsteps, 1)).transpose())
+    #VsubVth = np.subtract(Traj, VthMatH[::10,:].transpose())
+
+    # Motor Neurons 
+    Nplot = np.array([165, 153, 173, 189, 204, 219, 236, 164, 182, 196, 215, 
+                     232, 246, 151, 139, 171, 180, 187, 194, 203, 213, 218,
+                     230, 235, 167, 169, 175, 184, 191, 201, 206, 216, 222,
+                     233, 239, 242, 251])
+
+    # Subtract 1 since python matrices have 0 index
+    Nplot = np.subtract(Nplot, 1)
+
+    Motor_Vth = VsubVth[Nplot, 100:]
+    Motor_VthT = Motor_Vth.transpose()
+
+    # Non-modified voltage output
+    #Motor_Pure = Traj[Nplot, :]
+
+    # Perform SVD 
+    U, s, Z = np.linalg.svd(Motor_VthT, full_matrices=True)
+
+    fig = plt.figure(figsize=(9,6))
+    plt.plot(t[100:], U[:,0], t[100:], U[:,1], lw = 2)
+    plt.title('Motor Neurons: First Two Dominant Modes Dynamics', fontsize = 10)
+    plt.xlabel('Time (Seconds)')
+    plt.show
+    plt.savefig('MotorNeurons')
+
+    fig = plt.figure(figsize=(9,6))
+    plt.scatter(U[:,0], U[:,1])
+    plt.title('Phase Space of Two Modes', fontsize = 10)
+    plt.show
+    plt.savefig('PhaseSpace')
+    
+    #plt.scatter(U1, U2)
+    #plt.show
+    #plt.savefig('PhaseSpace')
+
+    #plt.plot(t[100:], U1, t[100:], U2, lw = 2)
+    #plt.show
+    #plt.savefig('MotorNeurons')
+
---
+++ app.py
@@ -0,0 +1,59 @@
+from gevent import monkey
+monkey.patch_all()
+
+import time
+import random
+import CElegansFull as CE
+import numpy as np
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask.ext.socketio import SocketIO, emit, join_room, leave_room, close_room, disconnect
+    
+(t, data, t_comp) = CE.run_Network(CE.Neuron, 0, 32.0, 0.01, 'PLM', 21000, 1e-3)
+data = data.transpose()
+simData = np.subtract(data, np.tile(CE.Vth, (CE.nsteps, 1)).transpose())
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+thread = None
+
+def send_simData(a,b):
+    for i in range(100,3200):
+        emit('new data', simData[:,i].tolist())
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        time.sleep(10)
+        count += 1
+        socketio.emit('my response',
+                      {'data': 'Server generated event', 'count': count},
+                      namespace='/test')
+
+@app.route('/')
+def index():
+    global thread
+    if thread is None:
+        thread = Thread(target=background_thread)
+        thread.start()
+    return render_template('index.html')
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': open("chem.json").read(), 'count': 0})
+
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('startRun', namespace='/test')
+def start_run(a,b):
+    send_simData(a,b)
+
+if __name__ == '__main__':
+    socketio.run(app)
+
